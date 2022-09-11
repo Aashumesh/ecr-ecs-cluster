5407949d2b5ec6b1671d9b7c234fe8e9991780b0
Aashumesh <98010730+Aashumesh@users.noreply.github.com>
2022-09-12 00:04:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./persistence');
 const getItems = require('./routes/getItems');
-const addItem = require('./routes/addItem');
+const addItems = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
 
@@ -10,7 +10,7 @@ app.use(require('body-parser').json());
 app.use(express.static(__dirname + '/static'));
 
 app.get('/items', getItems);
-app.post('/items', addItem);
+app.post('/items', addItems);
 app.put('/items/:id', updateItem);
 app.delete('/items/:id', deleteItem);
 
